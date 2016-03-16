9c384c02c1b18a81e04620975f451142f710d3ff
Daniel Tsui <tsui.daniel@gmail.com>
2016-03-16 15:38:12 +1100

    arrays

---
+++ 01-javascript/arrays/arrays.js
@@ -0,0 +1,15 @@
+console.log('arrays');
+
+var language1 = 'javascript';
+var language2 = 'ruby';
+
+var languages = ['javascript','ruby','php'];
+
+var colors = ['tomato','plum','pink'];
+
+console.log(colors[0]);
+console.log( colors[1] );
+
+for (var index = 0; index < colors.length; index++) {
+  console.log( colors[index] );
+}
