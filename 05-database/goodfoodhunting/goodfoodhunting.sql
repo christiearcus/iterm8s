1143d129806a767eeb1d4deb0271ecfec2f5af4d
Daniel Tsui <tsui.daniel@gmail.com>
2016-04-12 15:27:28 +1000

    for flo

--- 05-database/goodfoodhunting/goodfoodhunting.sql
+++ 05-database/goodfoodhunting/goodfoodhunting.sql
@@ -11,3 +11,5 @@ CREATE TABLE dishes (
 SELECT * FROM dishes;
 
 INSERT INTO dishes (name, image_url) VALUES ('prime ribs', 'http://www.simplyrecipes.com/wp-content/uploads/2006/12/prime-rib-640-dm.jpg');
+
+INSERT INTO dishes (name, image_url) VALUES ('crab', 'http://s3.amazonaws.com/foodspotting-ec2/reviews/5744706/thumb_600.jpg?1455696744?1460425011');
--- 05-database/goodfoodhunting/main.rb
+++ 05-database/goodfoodhunting/main.rb
@@ -1,16 +1,47 @@
-     
 require 'sinatra'
+require 'sinatra/reloader'
 require 'pg'
 
-get '/' do
+def run_sql(sql)
   db = PG.connect(dbname: 'goodfoodhunting')
-  rows = db.exec('SELECT * FROM dishes;')
+  results = db.exec(sql)
+  db.close
+  return results
+end
 
-  @first_dish_name = rows[0]['name']
-
+# list all dish
+get '/' do # request
+  @dishes = run_sql('SELECT * FROM dishes;')
   erb :index
 end
 
+# show new dish form
+get '/dishes/new' do
+  erb :new
+end 
+
+# creat a new dish
+post '/dishes' do
+  sql = "INSERT INTO dishes (name, image_url) VALUES ('#{ params[:name] }','#{ params[:image_url] }');"
+  run_sql(sql)
+  redirect to '/'
+end
+
+# show single dish
+get '/dishes/:id' do
+  sql = "SELECT * FROM dishes where id = #{ params[:id] };"
+  results = run_sql(sql)
+  @dish = results.first
+  erb :show
+end
+
+# update existing dish
+get '/dishes/:id/edit' do
+  sql = "SELECT * FROM dishes where id = #{ params[:id] };"
+  results = run_sql(sql) # database returns a array like collection always
+  @dish = results.first
+  erb :edit
+end
 
 
 
