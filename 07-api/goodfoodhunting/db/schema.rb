98ca154140215df94dd70368d1be28c3a08e1dca
Daniel Tsui <tsui.daniel@gmail.com>
2016-04-28 11:35:14 +1000

    tinder cake

--- 07-api/goodfoodhunting/db/schema.rb
+++ 07-api/goodfoodhunting/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427054124) do
+ActiveRecord::Schema.define(version: 20160428005102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,22 @@ ActiveRecord::Schema.define(version: 20160427054124) do
     t.integer  "dish_type_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "dish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["dish_id"], name: "index_likes_on_dish_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "likes", "dishes"
+  add_foreign_key "likes", "users"
 end
