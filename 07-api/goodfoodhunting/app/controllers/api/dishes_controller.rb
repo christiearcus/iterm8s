47e8d024d61b8d7b6f6add498b78d33d0b03d934
Daniel Tsui <tsui.daniel@gmail.com>
2016-05-02 14:19:51 +1000

    including create method

--- 07-api/goodfoodhunting/app/controllers/api/dishes_controller.rb
+++ 07-api/goodfoodhunting/app/controllers/api/dishes_controller.rb
@@ -6,5 +6,15 @@ module Api
       render json: @dishes.to_json
     end
 
+    def create
+      dish = Dish.new
+      dish.name = params[:name]
+      dish.image_url = params[:image_url]
+      if dish.save
+        render json: dish.to_json, status: 201
+      else
+      end
+    end
+
   end
 end
